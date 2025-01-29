d433d0d7e0ef9f7288a84cc6ddce34b834a2483b
Alexander0908 <sanya090895@gmail.com>
2025-01-29 13:09:18 +0200

    first commit

---
+++ test/index.ts
@@ -0,0 +1,39 @@
+const isBirthdayData: boolean = true;
+let ageData: number = 40;
+const userNameData: string = "John";
+
+const userData = {
+  isBirthdayData: true,
+  ageData: 40,
+  userNameData: "John",
+  messages: {
+    error: "Error"
+  }
+}
+
+const createError = (msg: string) => {
+  throw new Error(msg);
+};
+
+function logBrtMsg({isBirthdayData, userNameData, ageData, messages: {error}}: {
+  isBirthdayData: boolean, 
+  userNameData: string, 
+  ageData: number,
+  messages: {error: string}
+}): string {
+  if (isBirthdayData) {
+    return `Congrats ${userNameData.toUpperCase()}, age: ${ageData + 1}`;
+  } else {
+    return createError(error);
+  }
+}
+
+console.log(logBrtMsg(userData));
+
+const departments: string[] = ["dev", "design", "marketing"];
+
+const report = departments
+                          .filter(d => d !== "dev")
+                          .map(d => `${d} - done`);
+
+const nums: number[][] = [[1, 2, 3], [1, 2, 3]];
