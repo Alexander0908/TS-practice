2cf382af330d1ad2edf45bfe1e9a40ee9a83441a
Alexander0908 <sanya090895@gmail.com>
2025-01-29 14:05:38 +0200

    Narrowing

--- test/index.ts
+++ test/index.ts
@@ -42,3 +42,51 @@ const report = departments
                           .map((d: string) => `${d} - done`);
 
 const nums: number[][] = [[1, 2, 3], [1, 2, 3]];
+
+
+////////////////////////
+
+const message: string | number = 5;
+const messages: string[] | number[] = ["a", "b"];
+
+// function printMsg(msg: string | number | boolean):void {
+//   if (typeof msg === "string" || typeof msg === "number") {
+//     console.log(msg.toString());
+//   } else {
+//     console.log(msg);
+//   }
+// }
+
+function printMsg(msg: string[] | number | boolean):void {
+  if (Array.isArray(msg)) {
+    msg.forEach(m => console.log(m));
+  } else if (typeof msg === "number") {
+    console.log(msg.toFixed());
+  } else {
+    console.log(msg);
+  }
+}
+
+printMsg(4);
+
+const printReadings = (a: number | string, b: number | boolean) => {
+  if (a === b) {
+    console.log(a, b);
+  }
+}
+
+const checkReadings (readings: {system: number} | {user: number}): void => {
+  if ("system" in readings) {
+    console.log(readings.system);
+  } else {
+    console.log(readings.user);
+  }
+}
+
+function logValue (x: string | Date) {
+  if (x instanceof Date) {
+    console.log(x.getDate());
+  } else {
+    console.log(x.trim());
+  }
+}
