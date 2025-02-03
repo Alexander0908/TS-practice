eb141e4279a0ab9c50f5310e1c70a19052e0ec57
Alexander0908 <sanya090895@gmail.com>
2025-02-03 16:09:17 +0200

    interfaces

---
+++ 24/index-24.ts
@@ -0,0 +1,58 @@
+// type Config = {protocol: "http" | "https"; port: 3000 | 3001};
+interface Config {
+    protocol: "http" | "https";
+    port: 3000 | 3001;
+    log: (msg: string) => void;
+}
+
+// type Role = {
+//   role: string;
+// };
+// type ConfigWithRole = Config & Role;
+
+interface Role {
+    role: string;
+}
+
+interface ConfigWithRole extends Config, Role {}
+
+const serverConfig: ConfigWithRole = {
+  protocol: "https",
+  port: 3001,
+  role: 'admin',
+  log: (msg: string): void => console.log(msg)
+};
+
+// const backupConfig: ConfigWithRole = {
+//   protocol: "http",
+//   port: 3000,
+//   role: 'sysadmin'
+// }
+
+type StartFunction = (
+    protocol: "http" | "https", 
+    port: 3000 | 3001, 
+    log: (msg: string) => void
+) => string;
+
+const startServer: StartFunction = (
+  protocol: "http" | "https",
+  port: 3000 | 3001,
+  log: (msg: string) => void
+): "Server started" => {
+  log(`Server started on ${protocol}://server:${port}`);
+
+  return "Server started";
+}
+
+startServer(serverConfig.protocol, serverConfig.port, serverConfig.log);
+
+interface Styles {
+  [key: string]: string
+}
+
+const styles: Styles = {
+  position: 'absolute',
+  top: '20px',
+  left: '50px',
+}
---
+++ 24/test.ts
@@ -0,0 +1,25 @@
+const serverNewConfig = {
+    protocol: "https",
+    port: 3001,
+    role: 'admin',
+    log: (msg: string): void => console.log(msg)
+};
+
+const backupConfig = {
+    protocol: "http",
+    port: 3000,
+};
+
+interface BasicConfig {
+    protocol: string;
+    port: number;
+}
+
+const startNewServer = (config: BasicConfig): "Server started" => {
+    console.log(`Server started on ${config.protocol}://server:${config.port}`);
+  
+    return "Server started";
+};
+  
+startNewServer(serverNewConfig);
+startNewServer(backupConfig);
