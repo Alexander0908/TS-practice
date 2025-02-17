1f883d8fe484a1b783fc86bf869188b1bcf0f88e
Alexander0908 <sanya090895@gmail.com>
2025-02-17 14:36:17 +0200

    optional operator and turtle game
    optional operator and turtle game

---
+++ turtle-game/index.ts
@@ -0,0 +1,29 @@
+type Turtle = 'turtle' | null;
+
+type Game = {
+  makeTurn: (direction: 'left' | 'right') => void;
+  state: Array<Turtle>;
+};
+
+const startGame = (): Game => {
+  const state: Array<Turtle> = ['turtle', null, null, null, null];
+
+  // BEGIN (write your solution here)
+  const makeTurn = (direction: 'left' | 'right'): void => {
+    const turtleIndex = state.indexOf('turtle');
+    const nextIndex = direction === 'left' ? turtleIndex - 1 : turtleIndex + 1;
+
+    if (nextIndex < 0 || nextIndex > 4) {
+      throw new Error('Out of bounds');
+    }
+
+    state[turtleIndex] = null;
+    state[nextIndex] = 'turtle';
+  }
+  
+  // END
+
+  return { makeTurn, state };
+};
+
+export default startGame;
